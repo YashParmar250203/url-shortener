f14992509dd25d1dcac11f42d96a9a4b4e8d2b41
YashParmar250203 <155385741+YashParmar250203@users.noreply.github.com>
2025-06-27 00:19:03 +0530

    dashboard

--- url-shortener-frontend/src/main.jsx
+++ url-shortener-frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'; // ✅ ADD THIS LINE
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { ContextProvider } from './contextApi/ContextApi.jsx';
+import { QueryClient } from 'react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client = {queryClient}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
